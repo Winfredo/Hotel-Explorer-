5cc5297436d81b7963173cd80688173cb445606d
winfredo <winfrednukpe2002@gmail.com>
2023-10-28 11:45:54 -0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import React from 'react';
+// eslint-disable-next-line
 import * as ReactDOM from "react-dom/client";
 import Navbar from './components/Navbar/Navbar';
+// eslint-disable-next-line
+import { router } from './utils';
 
 
 function App() {
