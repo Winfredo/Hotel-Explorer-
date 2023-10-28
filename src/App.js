02fa8392d5368bba623f4adca0f2cedd3f1ad3ba
winfredo <winfrednukpe2002@gmail.com>
2023-10-28 13:04:11 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import './App.css';
 import React from 'react';
 // eslint-disable-next-line
 import * as ReactDOM from "react-dom/client";
-import Navbar from './components/Navbar/Navbar';
 // eslint-disable-next-line
 import { router } from './utils';
 
@@ -17,16 +14,11 @@ function App() {
 
   return (
 
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-  );
+   <div>
+  <Navbar />
+   </div>
+  );    
+
 }
 
 export default App;
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,11 +2,17 @@ import {
     createBrowserRouter,
   } from "react-router-dom";
 import App from "../App";
+import HeroSection from "../components/HeroSection/HeroSection";
+
 
 export const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
     },
+    {
+      path: "/herosection",
+      element: <HeroSection />
+    }
    
   ]);
